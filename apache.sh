534079faa64760f76aa9b26234c9a9e8ff1a0ba5
divyanagarathinam <divyanagarathinam@gmail.com>
2026-03-01 19:57:49 -0500

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -1,14 +1,7 @@
-#!/bin/bash
-apt update -y
-apt install -y apache2 git
-
-systemctl enable apache2
-systemctl start apache2
-
+#! /bin/bash
+yum install httpd git -y
+systemctl start httpd
+systemctl status httpd
 cd /var/www/html
 git clone https://github.com/karishma1521success/swiggy-clone.git
 mv swiggy-clone/* .
-rm -rf swiggy-clone
-
-chown -R www-data:www-data /var/www/html
-chmod -R 755 /var/www/html
