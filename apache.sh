dd258e9fe78167b881203a841536b0390967e01d
divyanagarathinam <divyanagarathinam@gmail.com>
2026-03-01 19:32:26 -0500

    Switch from yum to apt for package management

--- apache.sh
+++ apache.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-yum update -y
-yum install -y httpd git
+apt update -y
+apt install -y apache2 git
 
-systemctl enable httpd
-systemctl start httpd
+systemctl enable apache2
+systemctl start apache2
 
 cd /var/www/html
 git clone https://github.com/karishma1521success/swiggy-clone.git
-
 mv swiggy-clone/* .
 rm -rf swiggy-clone
 
-chown -R apache:apache /var/www/html
+chown -R www-data:www-data /var/www/html
 chmod -R 755 /var/www/html
