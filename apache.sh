bcfd86990d8dadef15cc74a5a41648fde13d7715
divyanagarathinam <divyanagarathinam@gmail.com>
2026-03-01 19:18:39 -0500

    Enhance apache.sh to enable httpd and update packages
    Updated script to include systemctl enable for httpd and added yum update before installations.

--- apache.sh
+++ apache.sh
@@ -1,7 +1,15 @@
-#! /bin/bash
-yum install httpd git -y
+#!/bin/bash
+yum update -y
+yum install -y httpd git
+
+systemctl enable httpd
 systemctl start httpd
-systemctl status httpd
+
 cd /var/www/html
 git clone https://github.com/karishma1521success/swiggy-clone.git
+
 mv swiggy-clone/* .
+rm -rf swiggy-clone
+
+chown -R apache:apache /var/www/html
+chmod -R 755 /var/www/html
